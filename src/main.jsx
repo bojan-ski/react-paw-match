0c07de9870b4d99b6bc9c879b7e959007fad784b
Bojan SKI <codeucenje@gmail.com>
2024-08-26 09:16:44 +0200

    react router dom and bootstrap installed. Google Poppins font and initail setup in index.css done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// custom css
 import './index.css'
+// bootstrap css
+import "/node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+// app
+import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
